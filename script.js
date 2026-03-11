c4aa7f8bb6f8337d3d71c17d9fc47147578fb40e
PHP2301 <phamphuoclk23012005@gmail.com>
2026-03-12 01:48:53 +0700

    123

---
+++ script.js
@@ -0,0 +1,6 @@
+// Giả sử bạn đã khởi tạo Firebase ở đây
+function saveToFirebase() {
+  // Đoạn code này sẽ gửi dữ liệu lên Firestore
+  console.log("Đã nhấn nút! Bạn có thể code gửi email khách hàng lên Firestore tại đây.");
+  alert("Cảm ơn bạn đã quan tâm đến Circle K!");
+}
